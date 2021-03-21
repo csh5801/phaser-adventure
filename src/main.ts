d4b8f7af30e5b73bfe49bdd1e6878171e183da80
Daniel <enddl5801@naver.com>
2021-03-21 18:33:18 +0900

    Added game.ts

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,13 @@
-const person: string = "Alice";
-console.log(person);
+import Phaser from "phaser";
+import Game from "./scenes/Game";
+
+export default new Phaser.Game({
+    type: Phaser.AUTO,
+    width: 400,
+    height: 250,
+    zoom: 2,
+    physics: {
+        default: "arcade",
+    },
+    scene: [Game],
+});
