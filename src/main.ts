6104f58517962c22ec4e275b9a84425aa50e392b
Josh Cooper <j@cooper.cx>
2021-03-28 11:04:31 +0100

    setup

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import Phaser from "phaser";
 import Game from "./scenes/Game";
 
 export default new Phaser.Game({
-    type: Phaser.AUTO,
-    width: 400,
-    height: 250,
-    zoom: 2,
-    physics: {
-        default: "arcade",
-    },
-    scene: [Game],
+  type: Phaser.AUTO,
+  width: 16 * 35,
+  height: 16 * 25,
+  zoom: 2,
+  physics: {
+    default: "arcade",
+  },
+  scene: [Game],
 });
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,13 +1,19 @@
 import Phaser from "phaser";
 
 export default class GameScene extends Phaser.Scene {
-    constructor() {
-        super("game")
-    }
+  constructor() {
+    super("game")
+  }
 
-    preload() {
-        
-    }
+  preload() {
+    this.load.image("tiles", "/overworld.png");
+    this.load.tilemapTiledJSON("adventure", "/phaser-adventure.json");
+  }
 
-    create() {}
+  create() {
+    const map = this.make.tilemap({ key: "adventure" });
+    const tileset = map.addTilesetImage("overworld", "tiles");
+
+    map.createLayer("ground", tileset);
+  }
 }
